05ba5829701934ab1cac14ec2138e4042f27d58d
Abhi-boostin <abhimanyusinghworks@gmail.com>
2025-07-20 01:40:09 +0530

    updated the final step message

--- src/blocks/Components/Stepper/steps/StepFinal.jsx
+++ src/blocks/Components/Stepper/steps/StepFinal.jsx
@@ -4,7 +4,7 @@ export default function StepFinal() {
   return (
     <>
       <h2 className="text-2xl font-bold mb-2">All Done!</h2>
-      <p>Click on the complete button and see your mailbox. Over.</p>
+      <p>Relax for a while you'll get the list shortly - takes 10-15 mins.</p>
     </>
   );
 } 
